85c24886a01d52510510abc40fa2d568d904cc66
Christian Adell <chadell@gmail.com>
2021-05-07 10:06:38 +0200

    Add testing coverage to Job methods (#5)
    Extend test coverage to related Job methods

---
+++ nautobot_circuit_maintenance/jobs.py
@@ -0,0 +1,4 @@
+"""Circuit Maintenance plugin jobs."""
+from .handle_notifications.handler import HandleCircuitMaintenanceNotifications
+
+jobs = [HandleCircuitMaintenanceNotifications]
--- nautobot_circuit_maintenance/jobs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Circuit Maintenance jobs."""
-from .handle_notifications import HandleCircuitMaintenanceNotifications
-
-jobs = (HandleCircuitMaintenanceNotifications,)
-
-__all__ = ("jobs",)
