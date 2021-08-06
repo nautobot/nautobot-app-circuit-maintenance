bf8befc65ef4f8d77b4d5776d3b0ae621292eaf5
Christian Adell <chadell@gmail.com>
2021-08-06 17:23:48 +0200

    Add a fake migration step to trigger the update of the Custom fields

---
+++ nautobot_circuit_maintenance/migrations/0006_fake_migration_to_update_custom_fields.py
@@ -0,0 +1,13 @@
+# Generated manually just to trigger post_migration signal that is
+# used to create the Custom Fields
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("nautobot_circuit_maintenance", "0005_notificationsource__token"),
+    ]
+
+    operations = []
