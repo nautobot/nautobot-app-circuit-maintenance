c2d7e3bcdd0801e25d050c225cc6a6d89acd4bcc
Uros Bajzelj <uros@bajzelj.com>
2021-10-18 18:56:30 +0200

    Add the name for the section in Nautobot jobs

--- nautobot_circuit_maintenance/handle_notifications/handler.py
+++ nautobot_circuit_maintenance/handle_notifications/handler.py
@@ -20,6 +20,9 @@ from nautobot_circuit_maintenance.models import (
 from .sources import get_notifications, MaintenanceNotification
 
 
+name = "Circuit Maintenance"
+
+
 # pylint: disable=broad-except
 PLUGIN_SETTINGS = settings.PLUGINS_CONFIG.get("nautobot_circuit_maintenance", {})
 
