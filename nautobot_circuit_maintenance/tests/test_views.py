d0ce8b9e5c0b7bd1cb73cb9c52de7c2b127031b8
Jan Snasel <jan.snasel@networktocode.com>
2023-03-14 09:08:02 +0000

    fix: Failing CircuitMaintenanceTest

--- nautobot_circuit_maintenance/tests/test_views.py
+++ nautobot_circuit_maintenance/tests/test_views.py
@@ -48,6 +48,9 @@ class CircuitMaintenanceTest(ViewTestCases.PrimaryObjectViewTestCase):
         CircuitMaintenance.objects.create(
             name="UT-TEST-2", start_time="2020-10-05T10:00:00", end_time="2020-10-05 12:00:00"
         )
+        CircuitMaintenance.objects.create(
+            name="UT-TEST-3", start_time="2020-10-06 10:00:00", end_time="2020-10-06 12:00:00"
+        )
 
         cls.form_data = {
             "name": "UT-TEST-10",
