d248d9fa21a3260d1f2ce420306bff4dd2112752
Josh VanDeraa <jv@networktocode.com>
2023-05-25 07:19:41 -0500

    1.0 Release (#251)
    * Adds towncrier, 1.0 release.
    
    * pylint updates.
    
    * Changelog addition.
    
    * Adds towncrier python package.
    
    * Fixes YAML lint.
    
    * Updates for pylint.
    
    * Updates for no py3.7
    
    * Update .github/workflows/ci.yml
    
    Co-authored-by: Christian Adell <chadell@gmail.com>
    
    * Revert nautobot_config changes.
    
    * Town Crier updates
    
    * Fix nautobot_config format
    
    * Fix examples.
    
    * Fix pyproject differing config.
    
    * Removes Python3.8 min, updates compose.
    
    * Updates lock
    
    * Adds what I thought was there.
    
    ---------
    
    Co-authored-by: Christian Adell <chadell@gmail.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+<!-- markdownlint-disable MD024 -->
+
+# Changelog - Nautobot Plugin ChatOps
+
+The changelog is located on the [project release notes](https://docs.nautobot.com/projects/circuit-maintenance/en/latest/admin/release_notes/).
--- nautobot_circuit_maintenance/navigation.py
+++ nautobot_circuit_maintenance/navigation.py
@@ -1,42 +1,54 @@
 """Navigation for Circuit Maintenance."""
-from nautobot.extras.plugins import PluginMenuButton, PluginMenuItem
 from nautobot.utilities.choices import ButtonColorChoices
+from nautobot.core.apps import NavMenuAddButton, NavMenuGroup, NavMenuItem, NavMenuTab, NavMenuButton
+
+# For Nautobot 1.5.8, once that becomes the minimum:
+# from nautobot.apps.ui import (NavMenuTab, NavMenuGroup, NavMenuItem, NavMenuButton)
 
 menu_items = (
-    PluginMenuItem(
-        link="plugins:nautobot_circuit_maintenance:circuitmaintenance_overview",
-        link_text="Dashboard",
-        permissions=["nautobot_circuit_maintenance.view_circuitmaintenance"],
-    ),
-    PluginMenuItem(
-        link="plugins:nautobot_circuit_maintenance:circuitmaintenance_list",
-        link_text="Circuit Maintenances",
-        permissions=["nautobot_circuit_maintenance.view_circuitmaintenance"],
-        buttons=(
-            PluginMenuButton(
-                link="plugins:nautobot_circuit_maintenance:circuitmaintenance_add",
-                title="Add Circuit Maintenance",
-                icon_class="mdi mdi-plus-thick",
-                color=ButtonColorChoices.GREEN,
-                permissions=["nautobot_circuit_maintenance.add_circuitmaintenance"],
-            ),
-            PluginMenuButton(
-                link="plugins:nautobot_circuit_maintenance:circuitmaintenance_job",
-                title="Job to update Circuit Maintenance",
-                icon_class="mdi mdi-database-refresh",
-                color=ButtonColorChoices.GREEN,
-                permissions=["nautobot_circuit_maintenance.add_circuitmaintenance"],
+    NavMenuTab(
+        name="Circuits",
+        groups=(
+            NavMenuGroup(
+                name="Circuit Maintenance App",
+                weight=250,
+                items=(
+                    NavMenuItem(
+                        link="plugins:nautobot_circuit_maintenance:circuitmaintenance_overview",
+                        name="Dashboard",
+                        permissions=["nautobot_circuit_maintenance.view_circuitmaintenance"],
+                    ),
+                    NavMenuItem(
+                        link="plugins:nautobot_circuit_maintenance:circuitmaintenance_list",
+                        name="Circuit Maintenances",
+                        permissions=["nautobot_circuit_maintenance.view_circuitmaintenance"],
+                        buttons=(
+                            NavMenuAddButton(
+                                link="plugins:nautobot_circuit_maintenance:circuitmaintenance_add",
+                                title="Add Circuit Maintenance",
+                                permissions=["nautobot_circuit_maintenance.add_circuitmaintenance"],
+                            ),
+                            NavMenuButton(
+                                link="plugins:nautobot_circuit_maintenance:circuitmaintenance_job",
+                                title="Job to update Circuit Maintenance",
+                                icon_class="mdi mdi-language-python",
+                                button_class=ButtonColorChoices.BLUE,
+                                permissions=["nautobot_circuit_maintenance.add_circuitmaintenance"],
+                            ),
+                        ),
+                    ),
+                    NavMenuItem(
+                        link="plugins:nautobot_circuit_maintenance:rawnotification_list",
+                        name="Notifications",
+                        permissions=["nautobot_circuit_maintenance.view_circuitmaintenance"],
+                    ),
+                    NavMenuItem(
+                        link="plugins:nautobot_circuit_maintenance:notificationsource_list",
+                        name="Notification Sources",
+                        permissions=["nautobot_circuit_maintenance.view_notificationsource"],
+                    ),
+                ),
             ),
         ),
-    ),
-    PluginMenuItem(
-        link="plugins:nautobot_circuit_maintenance:rawnotification_list",
-        link_text="Notifications",
-        permissions=["nautobot_circuit_maintenance.view_circuitmaintenance"],
-    ),
-    PluginMenuItem(
-        link="plugins:nautobot_circuit_maintenance:notificationsource_list",
-        link_text="Notification Sources",
-        permissions=["nautobot_circuit_maintenance.view_notificationsource"],
     ),
 )
