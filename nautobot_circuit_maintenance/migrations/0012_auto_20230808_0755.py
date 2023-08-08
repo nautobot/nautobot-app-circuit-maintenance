e5dcf8e4513ce83e52bb4f96c0af6e5eb9c09a38
Jan Snasel <jan.snasel@networktocode.com>
2023-08-08 07:58:25 +0000

    fix: Formatting

--- nautobot_circuit_maintenance/migrations/0012_auto_20230808_0755.py
+++ nautobot_circuit_maintenance/migrations/0012_auto_20230808_0755.py
@@ -6,43 +6,43 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('nautobot_circuit_maintenance', '0011_mysql'),
+        ("nautobot_circuit_maintenance", "0011_mysql"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='circuitimpact',
-            name='impact',
-            field=models.CharField(default='OUTAGE', max_length=50),
+            model_name="circuitimpact",
+            name="impact",
+            field=models.CharField(default="OUTAGE", max_length=50),
         ),
         migrations.AlterField(
-            model_name='circuitmaintenance',
-            name='ack',
+            model_name="circuitmaintenance",
+            name="ack",
             field=models.BooleanField(default=False),
         ),
         migrations.AlterField(
-            model_name='circuitmaintenance',
-            name='description',
-            field=models.TextField(blank=True, default=''),
+            model_name="circuitmaintenance",
+            name="description",
+            field=models.TextField(blank=True, default=""),
         ),
         migrations.AlterField(
-            model_name='circuitmaintenance',
-            name='status',
-            field=models.CharField(default='TENTATIVE', max_length=50),
+            model_name="circuitmaintenance",
+            name="status",
+            field=models.CharField(default="TENTATIVE", max_length=50),
         ),
         migrations.AlterField(
-            model_name='note',
-            name='level',
-            field=models.CharField(default='INFO', max_length=50),
+            model_name="note",
+            name="level",
+            field=models.CharField(default="INFO", max_length=50),
         ),
         migrations.AlterField(
-            model_name='rawnotification',
-            name='parsed',
+            model_name="rawnotification",
+            name="parsed",
             field=models.BooleanField(default=False),
         ),
         migrations.AlterField(
-            model_name='rawnotification',
-            name='sender',
-            field=models.CharField(blank=True, default='', max_length=200),
+            model_name="rawnotification",
+            name="sender",
+            field=models.CharField(blank=True, default="", max_length=200),
         ),
     ]
