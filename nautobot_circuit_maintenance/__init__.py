a625aedc222778ea5688751d0c870bb29d7acf6b
bakebot <info@networktocode.com>
2023-10-20 09:22:03 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "develop",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-plugin-circuit-maintenance.git",
        "path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-circuit-maintenance",
        "repository_path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-circuit-maintenance",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@chadell @glennmatthews @pke11y @scetron",
            "full_name": "Network to Code, LLC",
            "email": "opensource@networktocode.com",
            "github_org": "nautobot",
            "plugin_name": "nautobot_circuit_maintenance",
            "verbose_name": "Circuit Maintenance",
            "plugin_slug": "nautobot-circuit-maintenance",
            "project_slug": "nautobot-plugin-circuit-maintenance",
            "repo_url": "https://github.com/nautobot/nautobot-plugin-circuit-maintenance",
            "base_url": "circuit-maintenance",
            "min_nautobot_version": "2.0.0",
            "max_nautobot_version": "2.9999",
            "camel_name": "NautobotCircuitMaintenance",
            "project_short_description": "Nautobot plugin to automatically handle Circuit Maintenances Notifications",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/circuit-maintenance/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/opt/ntc/drift-manager/outputs",
            "_repo_dir": "/opt/ntc/drift-manager/outputs/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "develop"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.BLACK"
        ],
        "baked_commit_ref": "",
        "draft": true
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "./outputs",
        "push": true,
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "template_dir": "nautobot-app",
        "template_ref": "develop",
        "pull_request": null,
        "post_action": [
            "black"
        ],
        "disable_post_actions": false,
        "draft": true
    }
    ```

--- nautobot_circuit_maintenance/__init__.py
+++ nautobot_circuit_maintenance/__init__.py
@@ -1,10 +1,6 @@
 """Plugin declaration for nautobot_circuit_maintenance."""
 # Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
-try:
-    from importlib import metadata
-except ImportError:
-    # Python version < 3.8
-    import importlib_metadata as metadata
+from importlib import metadata
 
 __version__ = metadata.version(__name__)
 
@@ -21,8 +17,8 @@ class NautobotCircuitMaintenanceConfig(NautobotAppConfig):
     description = "Nautobot plugin to automatically handle Circuit Maintenances Notifications."
     base_url = "circuit-maintenance"
     required_settings = []
-    min_version = "1.4.0"
-    max_version = "1.9999"
+    min_version = "2.0.0"
+    max_version = "2.9999"
     default_settings = {}
     caching_config = {}
 
