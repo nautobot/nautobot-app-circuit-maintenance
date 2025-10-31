fbb72f8b81130b11f17118f7a7946008b11a2c26
Pavan-183 <pavan.pulicherla@networktocode.com>
2025-10-31 19:40:28 +0000

    Fixed pylint issue

--- nautobot_circuit_maintenance/views.py
+++ nautobot_circuit_maintenance/views.py
@@ -524,7 +524,7 @@ class NotificationSourceUIViewSet(NautobotUIViewSet):
         custom_view_base_action="view",
         custom_view_additional_permissions=["nautobot_circuit_maintenance.view_notificationsource"],
     )
-    def validate_source(self, request):
+    def validate_source(self, request, pk=None):  # pylint: disable=unused-argument
         """Validate NotificationSource authentication and render result directly for tests."""
         instance = self.get_object()
         source = None
