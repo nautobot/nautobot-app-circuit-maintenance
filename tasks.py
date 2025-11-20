2dbd5bce0adf5cfb4c59b708a0192d6883e4d290
Gavin Acosta <155584291+Defiantearth@users.noreply.github.com>
2025-11-19 20:59:46 -0600

    Replace next with 3.0.0 in CI and Dev (#355)

--- tasks.py
+++ tasks.py
@@ -52,7 +52,7 @@ namespace = Collection("nautobot_circuit_maintenance")
 namespace.configure(
     {
         "nautobot_circuit_maintenance": {
-            "nautobot_ver": "next",  # TODO: Change to 3.0.0 after v3.0.0 is released
+            "nautobot_ver": "3.0.0",
             "project_name": "nautobot-circuit-maintenance",
             "python_ver": "3.12",
             "local": False,
