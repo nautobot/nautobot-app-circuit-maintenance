1611afe90b75151c98a7a2dbb3e82c69fd022609
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2026-01-30 16:13:45 -0800

    Fixed upstream testing for django 5.2 by removing django-cryptography dependency and fixing migration that depended on it.

--- nautobot_circuit_maintenance/migrations/0001_initial.py
+++ nautobot_circuit_maintenance/migrations/0001_initial.py
@@ -5,7 +5,6 @@ import uuid
 import django.core.serializers.json
 import django.db.models.deletion
 import django.utils.timezone
-import django_cryptography.fields
 import taggit.managers
 from django.db import connection, migrations, models
 
@@ -130,7 +129,7 @@ class Migration(migrations.Migration):
                     "_custom_field_data",
                     models.JSONField(blank=True, default=dict, encoder=django.core.serializers.json.DjangoJSONEncoder),
                 ),
-                ("_password", django_cryptography.fields.encrypt(models.CharField(max_length=100))),
+                ("_password", models.CharField(max_length=100)),
                 ("source_id", models.EmailField(max_length=100, unique=True)),
                 ("url", models.CharField(max_length=200)),
                 ("source_type", models.CharField(blank=True, default="unknown", max_length=50, null=True)),
